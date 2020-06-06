f9111c770a520dae29e5c92b5b5d7f6efeb78647
Platinum <dominiclopez391@gmail.com>
2020-06-05 23:36:26 -0700

    change path

--- RimNudeWorld/patches/HarmonyPatch_GetErect.cs
+++ RimNudeWorld/patches/HarmonyPatch_GetErect.cs
@@ -40,35 +40,18 @@ namespace RimNudeWorld
                 }
 
                 if (pawn.Corpse != null && pawn.Corpse.CurRotDrawMode == RotDrawMode.Dessicated && ((originalPath.Length > 8 && originalPath.Contains("Genitals")) || (originalPath.Length > 7 && originalPath.Contains("Breasts")))) {
-                    __result = null;
+                    __result = GraphicDatabase.Get<Graphic_Multi>("[INVIS TEXTURE PATH HERE]", __result.Shader, __result.drawSize, __result.color, __result.colorTwo);
                 }
                 return;
 
             }
-
             else if (originalPath.Length >= 9 && originalPath.Contains("penis")) {
-                if (NudeSettings.debugMode) {
-                    Log.Message("flag 1");
-                }
 
                 string modifiedPath = originalPath.Insert(9, "Flaccid/") + "_flaccid";
 
-                if (NudeSettings.debugMode) {
-                    Log.Message("flag 2");
-                }
-
-
                 if (pawn.RaceHasSexNeed()) {
 
-                    if (NudeSettings.debugMode) {
-                        Log.Message("flag 3");
-                    }
-
                     if (xxx.need_sex(pawn) > xxx.SexNeed.Frustrated && !(pawn.jobs.curDriver is JobDriver_Sex) && ContentFinder<Texture2D>.Get(modifiedPath + "_north", false) != null) {
-
-                        if (NudeSettings.debugMode) {
-                            Log.Message("flag 4");
-                        }
 
                         Graphic newGraphic = GraphicDatabase.Get<Graphic_Multi>(modifiedPath, __result.Shader, __result.drawSize, __result.color, __result.colorTwo);
                         __result = newGraphic;
